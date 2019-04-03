9f2bc0cc60d2a292a090fb49167f46fac50c8454
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-03 06:27:53 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,27 +1,27 @@
 def addition(num1, num2)
-  1+1
+  8+1
 end
 
 def subtraction(num1, num2)
-  1-1
+  6-1
 end
 
 def division(num1, num2)
-  1/2.0
+  100/4
 end
 
 def multiplication(num1, num2)
-  1*1
+  10*20
 end
 
 def modulo(num1, num2)
-  Math.modulo(9)
+  10%5
 end
 
 def square_root(num)
-  Math.sqrt(2)
+  Math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  1 + (( 2 * 2 ) /  3 )
+  #Hint:  0 + (( 12 * 2 ) /  3 )
 end
